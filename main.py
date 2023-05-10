5862c3954d9a508cbe3cf71334c88fdbd2024162
Austin Johnson <austin@austinjohnson.me>
2023-05-10 13:40:29 -0500

    explicit schemas from LC

--- main.py
+++ main.py
@@ -2,8 +2,41 @@ from dotenv import load_dotenv
 
 import os
 
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
 load_dotenv()
 
+
+def main():
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        raise Exception("OPENAI_API_KEY is not set")
+    else:
+        print("OPENAI_API_KEY is set")
+
+    chat = ChatOpenAI(
+        temperature=0.5,
+        # model_name="gpt-4"
+    )
+
+    messages = [
+        SystemMessage(content="You are a helpful assistant"),
+    ]
+
+    print("Hello, I am a helpful CLI assistant. How can I help you?")
+
+    while True:
+        user_input = input("> ")
+
+        messages.append(HumanMessage(content=user_input))
+
+        ai_response = chat(messages)
+
+        messages.append(AIMessage(content=ai_response.content))
+
+        print("\nAssistant:\n", ai_response.content)
+
+
 if __name__ == '__main__':
-    print("hello world")
-    print('My API key is:', os.environ.get('OPENAI_API_KEY'))
+    main()
