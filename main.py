1c463027ee1c1d1699c225b615d4a7238ba7c619
Austin Johnson <austin@austinjohnson.me>
2023-05-10 13:53:35 -0500

    with conversationbuffermemory

--- main.py
+++ main.py
@@ -15,27 +15,21 @@ def main():
     else:
         print("OPENAI_API_KEY is set")
 
-    chat = ChatOpenAI(
-        temperature=0.5,
-        # model_name="gpt-4"
+    llm = ChatOpenAI()
+    conversation = ConversationChain(
+        llm=llm,
+        memory=ConversationBufferMemory(),
+        verbose=True
     )
-
-    messages = [
-        SystemMessage(content="You are a helpful assistant"),
-    ]
 
     print("Hello, I am a helpful CLI assistant. How can I help you?")
 
     while True:
         user_input = input("> ")
 
-        messages.append(HumanMessage(content=user_input))
+        ai_response = conversation.predict(input=user_input)
 
-        ai_response = chat(messages)
-
-        messages.append(AIMessage(content=ai_response.content))
-
-        print("\nAssistant:\n", ai_response.content)
+        print("\nAssistant:\n " + ai_response + "\n")
 
 
 if __name__ == '__main__':
